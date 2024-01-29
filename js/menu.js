6af4f35c0fb61ec2505c2fe73af2826001f3645b
joao vitor <145628900+desafiogamer@users.noreply.github.com>
2024-01-29 14:42:25 -0300

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,46 @@
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('.Menu3d li a');
+let navLinksMobi = document.querySelectorAll('.navbar a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(Links => {
+                Links.classList.remove('active');
+                document.querySelector('.Menu3d li a[href*=' + id + ']').classList.add('active');
+            });
+            navLinksMobi.forEach(Links => {
+                Links.classList.remove('active');
+                document.querySelector('.navbar a[href*=' + id + ']').classList.add('active');
+            });
+        }
+    });
+}
+
+let front = document.getElementById('front')
+let full = document.getElementById('full')
+let projetosFront = document.querySelector('.front-end')
+let projetosFull = document.querySelector('.full-stacks')
+
+front.addEventListener('click', ()=>{
+    projetosFront.classList.add('ativo')
+    projetosFull.classList.remove('ativo')
+    front.style.color = '#1E90FF'
+    full.style.color = '#ffffff'
+    front.classList.remove('ativo')
+})
+
+full.addEventListener('click', ()=>{
+    projetosFront.classList.remove('ativo')
+    projetosFull.classList.add('ativo')
+    full.style.color = '#1E90FF'
+    front.style.color = '#ffffff'
+    front.classList.remove('ativo')
+})
+
+
